e046ad47a78b87cc36a15317cec88106a0d1de5a
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-02-10 16:53:54 +0700

    Fix Error in Image

--- app/src/main/java/com/nurdin/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/nurdin/myrecyclerview/ListHeroAdapter.kt
@@ -1,13 +1,12 @@
 package com.nurdin.myrecyclerview
 
 import android.view.LayoutInflater
-import android.view.ScrollCaptureCallback
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>(){
     private lateinit var onItemClickCallback: OnItemClickCallback
@@ -29,13 +28,18 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .circleCrop()
+            .into(holder.imgPhoto)
         holder.tvName.text = name
         holder.tvDescription.text = description
 //        holder.itemView.setOnClickListener{
 //            Toast.makeText(holder.itemView.context, "Kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
 //        }
-        holder.itemView.setOnClickListener{ onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+        holder.itemView.setOnClickListener{
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
+        }
     }
 
     override fun getItemCount(): Int = listHero.size
